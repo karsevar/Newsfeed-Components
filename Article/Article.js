7068efd65cb3b813bd3b0f5971dffb9c06517c05
masonkarsevar <masonkarsevar@gmail.com>
2019-06-19 08:01:35 -0700

    MVP: expand and collapse functionality finished. WIP: extra articles.

--- Article/Article.js
+++ Article/Article.js
@@ -10,12 +10,12 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'EXPAND';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
