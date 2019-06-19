f876f8058602f64cde70ecfc111479561574aa38
masonkarsevar <masonkarsevar@gmail.com>
2019-06-19 12:44:44 -0700

    stretch problem #2 finished

--- Article/Article.js
+++ Article/Article.js
@@ -9,24 +9,35 @@ class Article {
     // console.log(this.expandButton) 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'EXPAND';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+
+    // Stretch problem #2 remove button:
+    // The following implementation works but still it is very memory 
+    // inefficient. Will need to think of a better way.
+    const deleteButton = document.createElement('span');
+    deleteButton.className = 'remove';
+    deleteButton.textContent = 'read';
+    this.domElement.prepend(deleteButton)
+
+    this.removeButton = this.domElement.querySelector('.remove');
+    // Setting a click handler for removeButton:
+    this.removeButton.addEventListener('click', this.removeArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
   }
+
+  // Following code is for stretch problem 2:
+  removeArticle() {
+    this.domElement.style.display = 'none';
+  }
 }
 
 // stretch article markup with DOM and class component:
-
-const articleObject = {
-  'domElement': document.querySelector('.articles'),
-  'header': 'Lambda School Students: "We\'re the best!"',
-  'date': 'Nov 5th, 2017',
-  'paragraph': 'Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2 windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba owen jinn tatooine sith organa.'
-}
 
 class ArticleMarkup {
   constructor(articleContents) {
@@ -38,6 +49,7 @@ class ArticleMarkup {
   }
 
   printMarkup() {
+
     // Creating the div layer <div class='article'>
     const container = document.createElement('div');
     container.className = 'article';
@@ -62,6 +74,7 @@ class ArticleMarkup {
     /* 
     Creating the following node tree 
     <div class='article'>
+      <span class='remove'></span> //add on for stretch #2
       <h2></h2>
       <p class='date'></p>
       <p></p>
@@ -69,6 +82,7 @@ class ArticleMarkup {
     </div>
     */
 
+    // container.appendChild(deleteButton);
     container.appendChild(headerElement);
     container.appendChild(dateElement);
     container.appendChild(paragraph);
@@ -90,7 +104,15 @@ class ArticleMarkup {
 // Nice the following ArticleMarkup class works perfectly it populates 
 // the html page with new .article markup. Will need to refactor this 
 // with the Article class if possible.
-new ArticleMarkup(articleObject);
+
+const articleObject1 = {
+  'domElement': document.querySelector('.articles'),
+  'header': 'Lambda School Students: "We\'re the best!"',
+  'date': 'Nov 5th, 2017',
+  'paragraph': 'Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2 windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba owen jinn tatooine sith organa.'
+}
+
+new ArticleMarkup(articleObject1);
 
 let articles = document.querySelectorAll('.article');
 console.log(articles);
